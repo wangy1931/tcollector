5c00d896fd56e1cef2d60b68400f488d2bd7dd49
lzy <347316019@qq.com>
2017-06-17 11:10:23 +0800

    修改mysql的deploy文件，将依赖移除，使用pymysql

--- deploy/collectors/mysql/deploy.sh
+++ deploy/collectors/mysql/deploy.sh
@@ -73,7 +73,6 @@ if [ ! -f "$mysql_conf_file" ]; then
 fi
 
 command -v mysql > /dev/null 2>&1 || { echo >&2 "mysql command does not exist.  Aborting."; exit 1; }
-command -v mysql_config > /dev/null 2>&1 || { echo >&2 "mysql_config command does not exist. Make sure it is in the path. Usually it is under <mysql_root>/bin. Easy fix is to create a symlink like ln -s <mysql_install_root>/bin/mysql_config /usr/bin/mysql_config. Aborting."; exit 1; }
 log_info "CREATE USER '${mysql_stats_user}'@'localhost' IDENTIFIED BY '${mysql_stats_pass}'"
 log_info "type in password for mysql user $mysql_priv_user"
 mysql -u "$mysql_priv_user" -p -e "GRANT USAGE ON *.* TO '${mysql_stats_user}'@'localhost'; DROP USER '${mysql_stats_user}'@'localhost'; GRANT PROCESS, REPLICATION CLIENT ON *.* TO '${mysql_stats_user}'@'localhost' IDENTIFIED BY '${mysql_stats_pass}'"
@@ -83,33 +82,5 @@ log_info "config mysql stats user/pass in ${mysql_conf_file} s/user:.*/user: ${m
 sed -i -e "s/enabled:.*/enabled: True/g" -e "s/user:.*/user: ${mysql_stats_user}/g" -e "s/pass:.*/pass: ${mysql_stats_pass}/g" ${mysql_conf_file}
 abort_if_failed "failed to config mysql collector conf file"
 
-if ! $("$agent_install_folder/altenv/bin/python" -c 'import MySQLdb' &> /dev/null); then
-    log_info "install MySQLdb python module to talk to mysql to collect data"
-    pushd "$agent_install_folder"
-    mkdir -p workspace
-    if [ -z "$libpath" ]; then
-        wget --directory-prefix=./workspace/ https://download.cloudwiz.cn/package/MySQL-python-1.2.5.zip#md5=654f75b302db6ed8dc5a898c625e030c
-        abort_if_failed 'failed to download https://download.cloudwiz.cn/package/MySQL-python-1.2.5.zip#md5=654f75b302db6ed8dc5a898c625e030c'
-    else
-        log_info "cp $libpath workspace/MySQL-python-1.2.5.zip"
-        cp "$libpath" workspace/MySQL-python-1.2.5.zip
-        abort_if_failed "failed to cp $libpath workspace/MySQL-python-1.2.5.zip"
-    fi
-    unzip workspace/MySQL-python-1.2.5.zip -d workspace/
-    abort_if_failed 'failed to unzip workspace/MySQL-python-1.2.5.zip'
-    cd workspace/MySQL-python-1.2.5
-    "$agent_install_folder/altenv/bin/python" setup.py install --prefix="$agent_install_folder/altenv" --record ../mysql-python-filelist.txt
-    abort_if_failed 'failed to install MySQL-python-1.2.5'
-    popd
 
-    if [ -z "${LD_LIBRARY_PATH// }" ]; then
-        read -p "LD_LIBRARY_PATH environment variable is not set, please type in. This should be your mysql/lib folder: " lib_so_path
-    else
-        log_info "LD_LIBRARY_PATH environment is set to be $LD_LIBRARY_PATH, use it"
-        lib_so_path=$LD_LIBRARY_PATH
-    fi
-    log_info "configure LD_LIBRARY_PATH in supervisor_conf_file, set it to be $lib_so_path"
-    sed -i -r "s|^environment=LD_LIBRARY_PATH *= *(,.*)*|environment=LD_LIBRARY_PATH=$lib_so_path\1|g" ${supervisor_conf_file}
-    abort_if_failed "failed to set LD_LIBRARY_PATH in supervisor_conf_file"
-fi
 log_info "Complete!"
