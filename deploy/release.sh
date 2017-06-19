afe202aa6cdcc839df78b65648bed387f85c3647
hehaiyuan <hehaiyuan.hhy@gmail.com>
2017-06-19 12:08:35 +0800

    add release.sh

---
+++ deploy/release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+
+pushd /root/src/tcollector/deploy
+git pull
+sudo ./build.sh -h /root/src/tcollector/
+popd
+
+scp -r /tmp/publish/RedHat root@172.17.0.1:/data/workspace/tcollector/release/<OS>-<version>-$current_time
