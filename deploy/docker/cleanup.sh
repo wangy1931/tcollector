3fc11cc997ef2af04515c522850b1034446a1f54
hehaiyuan <hehaiyuan.hhy@gmail.com>
2017-06-19 09:49:32 +0800

    build release in docker

--- deploy/docker/cleanup.sh
+++ deploy/docker/cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./container_rm_stopped.sh
-./image_rm_dangling.sh
+./image_rm_untagged.sh
---
+++ deploy/docker/run.centos.6.6.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./run_interactive.sh cloudwiz.cn/centos/6.6 /bin/bash
+
+exit $?
