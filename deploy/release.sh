205c3f17655fa132c19688851d4da02bd740fa8b
lzy <347316019@qq.com>
2017-06-26 17:18:07 +0800

    使用使用ＯＳ　name作为路径

--- deploy/release.sh
+++ deploy/release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 source common.sh
-OS=$1
-version=$2
+OS=$(get_os)
+version=$1
 pushd /root/src/tcollector/deploy
 git pull
 sudo ./build.sh -h /root/src/tcollector/
 popd
 
-scp -r /tmp/publish/RedHat root@172.17.0.1:/data/workspace/tcollector/release/${OS}-${version}-$current_time
+scp -r /tmp/publish/${OS} root@172.17.0.1:/data/workspace/tcollector/release/${OS}-${version}-$current_time
