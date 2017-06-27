49a2c27fa4f3d9efa821567e51ed5cba105cf2c9
lzy <347316019@qq.com>
2017-06-27 14:55:44 +0800

    fix a bug

--- deploy/release.sh
+++ deploy/release.sh
@@ -8,4 +8,4 @@ pushd /root/src/tcollector/deploy
 sudo ./build.sh -h /root/src/tcollector/
 popd
 
-scp -r /tmp/publish/RedHat root@172.17.0.1:/data/workspace/tcollector/release/${OS}-${version}-$current_time
+scp -r /tmp/publish/${OS} root@172.17.0.1:/data/workspace/tcollector/release/${OS}-${version}-$current_time
