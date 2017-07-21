a27b6a7b833040775ee51ba7ecc9726905396f73
lzy <347316019@qq.com>
2017-07-21 10:52:51 +0800

    修改写filebeat的yml的自动写入方式

--- deploy/filebeat_conf.py
+++ deploy/filebeat_conf.py
@@ -4,6 +4,25 @@ import os
 
 runner_config_path = os.path.split(os.path.realpath(__file__))[0]
 
+template="""
+- input_type: <input_type>
+
+  # Paths that should be crawled and fetched. Glob based paths.
+  paths:
+    #- <path>
+
+  document_type: <document_type>
+  fields.orgid: <orgid>
+  fields.sysid: <sysid>
+  fields.token: <token>
+  fields_under_root: true
+  close_inactive: 2h
+  tail_files: true
+  multiline.pattern: '<pattern>'
+  multiline.negate: true
+  multiline.match: after
+
+"""
 def load_runner_conf(path):
     runner_config_path = path
     # print runner_config_path
@@ -14,21 +33,21 @@ def load_runner_conf(path):
 def get_comman_dict():
     common = load_runner_conf(runner_config_path+'/common.conf')
 
-    return dict(
-    input_type="  input_type: %s" % common.get('conf', 'input_type'),
-    fileds_orgid = "  fields.orgid: %s" % common.get('conf', 'fields.orgid'),
-    fileds_sysid = "  fields.sysid: %s" % common.get('conf', 'fields.sysid'),
-    fileds_token = "  fields.token: %s" % common.get('conf', 'fields.token'),
-    ields_under_root = "  ields_under_root: %s" % common.get('conf', 'ields_under_root'),
-    tail_files = "  tail_files: %s" % common.get('conf', 'tail_files'),
-    multiline_negate = "  multiline.negate: %s" % common.get('conf', 'multiline.negate'),
-    multiline_match = "  multiline.match: %s" % common.get('conf', 'multiline.match'),
-    close_inactive= "  close_inactive: %s" % common.get('conf', 'close_inactive'),
-    )
+    return {
+    "<input_type>":common.get('conf', 'input_type'),
+    "<orgid>":common.get('conf', 'fields.orgid'),
+    "<sysid>":common.get('conf', 'fields.sysid'),
+    "<token>" :common.get('conf', 'fields.token')
+    }
 def get_user_conf():
     user =load_runner_conf(runner_config_path+'/user.conf')
     return eval(user.get('conf','logs'))
 
+def get_all_dict(common,user):
+    temp=common.copy()
+    for key in user:
+        temp["<%s>"%key]=user[key]
+    return temp
 def set_filebeat_yml(common,user_conf):
     write_date=""
     with open(runner_config_path+'/filebeat_template.yml') as f:
@@ -36,19 +55,15 @@ def set_filebeat_yml(common,user_conf):
              if "##NEXT" in line:
                  write_date += "#" + ("=" * 78) + "\n"
                  for user_info in user_conf:
-                     write_date += "-\n"
-                     for key in common:
-                         write_date += common[key] + "\n"
-                     for user_info_key in user_info:
-                         if 'path' in user_info_key:
-                             write_date += "  paths:\n"
-                             for p in user_info[user_info_key]:
-                                 write_date +='    - %s\n'%p
-                         elif 'document_type' in user_info_key:
-                             write_date += "  document_type: %s\n" % user_info[user_info_key]
-                         elif 'pattern' in user_info_key:
-                             write_date +="  multiline.pattern: \'%s\'\n"%user_info[user_info_key]
-
+                     temp=get_all_dict(common,user_info)
+                     temp_str=template
+                     for key in temp:
+                         if '<path>' == key:
+                             for path in temp[key]:
+                                 temp_str = temp_str.replace("#- <path>", "- %s\n    #- <path>"%path)
+                         else:
+                             temp_str=temp_str.replace(key,temp[key])
+                     write_date += temp_str
                  write_date+="##NEXT\n#"+("="*78)+"\n"
 
              else:
