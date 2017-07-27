f83f5dda4e385e3e1079a75640d60ba83d73a8a5
Lzy <347316109@qq.com>
2017-07-27 10:23:19 +0800

    整理依赖，将依赖放到lib文件jar

--- deploy/windows/windows_setup.py
+++ deploy/windows/windows_setup.py
@@ -2,12 +2,12 @@ from cx_Freeze import setup, Executable
 
 
 # collectors_files=["builtin","conf","etc","lib","__init__.py"]
-files=["runner.conf","lib/nssm.exe","deploy/windows/release","deploy/windows/windows_init.bat","deploy/workspace/winlogbeat-5.4.3-windows-x86_64"]
+files=["runner.conf","collectors/","logs/","runs/","lib/nssm.exe","deploy/windows/README.md","deploy/windows/windows_init.bat","deploy/workspace/filebeat-5.4.2-windows-x86_64"]
 
 
 buildOptions = dict(include_files=files)
 
-build_exe_options = {"packages": ["collectors"],"include_files":files}
+build_exe_options = {"packages": ["pyodbc","checks"],"include_files":files}
 
 executables = [
     Executable('runner.py'),
