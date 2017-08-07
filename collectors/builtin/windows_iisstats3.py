da7eb7befa5fccefaf58ab901271f8b37fc72203
Haiyuanhe <hehaiyuan.hhy@gmail.com>
2017-08-07 22:57:28 +0800

    add iis state

--- collectors/builtin/windows_iisstats3.py
+++ collectors/builtin/windows_iisstats3.py
@@ -42,101 +42,110 @@ class WindowsIisstats(CollectorBase):
         self.WMISampler = partial(WMISampler, logger)
 
     def __call__(self):
-        metrics = self.WMISampler("Win32_PerfFormattedData_W3SVC_WebService", \
-		    ["AnonymousUsersPersec", \
-			 "BytesReceivedPersec", \
-			 "BytesSentPersec", \
-			 "BytesTotalPersec", \
-			 "CGIRequestsPersec", \
-			 "ConnectionAttemptsPersec", \
-			 "CopyRequestsPersec", \
-			 "CurrentAnonymousUsers", \
-			 "CurrentBlockedAsyncIORequests", \
-			 "Currentblockedbandwidthbytes", \
-			 "CurrentCALcountforauthenticatedusers", \
-			 "CurrentCALcountforSSLconnections", \
-			 "CurrentCGIRequests", \
-			 "CurrentConnections", \
-			 "CurrentISAPIExtensionRequests", \
-			 "CurrentNonAnonymousUsers", \
-			 "FilesPersec", \
-			 "FilesReceivedPersec", \
-			 "FilesSentPersec", \
-			 "GetRequestsPersec", \
-			 "HeadRequestsPersec", \
-			 "ISAPIExtensionRequestsPersec", \
-			 "LockedErrorsPersec", \
-			 "LockRequestsPersec", \
-			 "LogonAttemptsPersec", \
-			 "MaximumAnonymousUsers", \
-			 "MaximumCALcountforauthenticatedusers", \
-			 "MaximumCALcountforSSLconnections", \
-			 "MaximumCGIRequests", \
-			 "MaximumConnections", \
-			 "MaximumISAPIExtensionRequests", \
-			 "MaximumNonAnonymousUsers", \
-			 "MeasuredAsyncIOBandwidthUsage", \
-			 "MkcolRequestsPersec", \
-			 "MoveRequestsPersec", \
-			 "NonAnonymousUsersPersec", \
-			 "NotFoundErrorsPersec", \
-			 "OptionsRequestsPersec", \
-			 "OtherRequestMethodsPersec", \
-			 "PostRequestsPersec", \
-			 "PropfindRequestsPersec", \
-			 "ProppatchRequestsPersec", \
-			 "PutRequestsPersec", \
-			 "SearchRequestsPersec", \
-			 "ServiceUptime", \
-			 "TotalAllowedAsyncIORequests", \
-			 "TotalAnonymousUsers", \
-			 "TotalBlockedAsyncIORequests", \
-			 "Totalblockedbandwidthbytes", \
-			 "TotalBytesReceived", \
-			 "TotalBytesSent", \
-			 "TotalBytesTransferred", \
-			 "TotalCGIRequests", \
-			 "TotalConnectionAttemptsallinstances", \
-			 "TotalCopyRequests", \
-			 "TotalcountoffailedCALrequestsforauthenticatedusers", \
-			 "TotalcountoffailedCALrequestsforSSLconnections", \
-			 "TotalDeleteRequests", \
-			 "TotalFilesReceived", \
-			 "TotalFilesSent", \
-			 "TotalFilesTransferred", \
-			 "TotalGetRequests", \
-			 "TotalHeadRequests", \
-			 "TotalISAPIExtensionRequests", \
-			 "TotalLockedErrors", \
-			 "TotalLockRequests", \
-			 "TotalLogonAttempts", \
-			 "TotalMethodRequests", \
-			 "TotalMethodRequestsPersec", \
-			 "TotalMkcolRequests", \
-			 "TotalMoveRequests", \
-			 "TotalNonAnonymousUsers", \
-			 "TotalNotFoundErrors", \
-			 "TotalOptionsRequests", \
-			 "TotalOtherRequestMethods", \
-			 "TotalPostRequests", \
-			 "TotalPropfindRequests", \
-			 "TotalProppatchRequests", \
-			 "TotalPutRequests", \
-			 "TotalRejectedAsyncIORequests", \
-			 "TotalSearchRequests", \
-			 "TotalTraceRequests", \
-			 "TotalUnlockRequests", \
-			 "TraceRequestsPersec", \
-			 "UnlockRequestsPersec"], \
-			 provider="64", timeout_duration=50)
-        metrics.sample()
-        ts = int(time.time())
+        iis_alive = False
+        try:
+            metrics = self.WMISampler("Win32_PerfFormattedData_W3SVC_WebService", \
+                                      ["AnonymousUsersPersec", \
+                                       "BytesReceivedPersec", \
+                                       "BytesSentPersec", \
+                                       "BytesTotalPersec", \
+                                       "CGIRequestsPersec", \
+                                       "ConnectionAttemptsPersec", \
+                                       "CopyRequestsPersec", \
+                                       "CurrentAnonymousUsers", \
+                                       "CurrentBlockedAsyncIORequests", \
+                                       "Currentblockedbandwidthbytes", \
+                                       "CurrentCALcountforauthenticatedusers", \
+                                       "CurrentCALcountforSSLconnections", \
+                                       "CurrentCGIRequests", \
+                                       "CurrentConnections", \
+                                       "CurrentISAPIExtensionRequests", \
+                                       "CurrentNonAnonymousUsers", \
+                                       "FilesPersec", \
+                                       "FilesReceivedPersec", \
+                                       "FilesSentPersec", \
+                                       "GetRequestsPersec", \
+                                       "HeadRequestsPersec", \
+                                       "ISAPIExtensionRequestsPersec", \
+                                       "LockedErrorsPersec", \
+                                       "LockRequestsPersec", \
+                                       "LogonAttemptsPersec", \
+                                       "MaximumAnonymousUsers", \
+                                       "MaximumCALcountforauthenticatedusers", \
+                                       "MaximumCALcountforSSLconnections", \
+                                       "MaximumCGIRequests", \
+                                       "MaximumConnections", \
+                                       "MaximumISAPIExtensionRequests", \
+                                       "MaximumNonAnonymousUsers", \
+                                       "MeasuredAsyncIOBandwidthUsage", \
+                                       "MkcolRequestsPersec", \
+                                       "MoveRequestsPersec", \
+                                       "NonAnonymousUsersPersec", \
+                                       "NotFoundErrorsPersec", \
+                                       "OptionsRequestsPersec", \
+                                       "OtherRequestMethodsPersec", \
+                                       "PostRequestsPersec", \
+                                       "PropfindRequestsPersec", \
+                                       "ProppatchRequestsPersec", \
+                                       "PutRequestsPersec", \
+                                       "SearchRequestsPersec", \
+                                       "ServiceUptime", \
+                                       "TotalAllowedAsyncIORequests", \
+                                       "TotalAnonymousUsers", \
+                                       "TotalBlockedAsyncIORequests", \
+                                       "Totalblockedbandwidthbytes", \
+                                       "TotalBytesReceived", \
+                                       "TotalBytesSent", \
+                                       "TotalBytesTransferred", \
+                                       "TotalCGIRequests", \
+                                       "TotalConnectionAttemptsallinstances", \
+                                       "TotalCopyRequests", \
+                                       "TotalcountoffailedCALrequestsforauthenticatedusers", \
+                                       "TotalcountoffailedCALrequestsforSSLconnections", \
+                                       "TotalDeleteRequests", \
+                                       "TotalFilesReceived", \
+                                       "TotalFilesSent", \
+                                       "TotalFilesTransferred", \
+                                       "TotalGetRequests", \
+                                       "TotalHeadRequests", \
+                                       "TotalISAPIExtensionRequests", \
+                                       "TotalLockedErrors", \
+                                       "TotalLockRequests", \
+                                       "TotalLogonAttempts", \
+                                       "TotalMethodRequests", \
+                                       "TotalMethodRequestsPersec", \
+                                       "TotalMkcolRequests", \
+                                       "TotalMoveRequests", \
+                                       "TotalNonAnonymousUsers", \
+                                       "TotalNotFoundErrors", \
+                                       "TotalOptionsRequests", \
+                                       "TotalOtherRequestMethods", \
+                                       "TotalPostRequests", \
+                                       "TotalPropfindRequests", \
+                                       "TotalProppatchRequests", \
+                                       "TotalPutRequests", \
+                                       "TotalRejectedAsyncIORequests", \
+                                       "TotalSearchRequests", \
+                                       "TotalTraceRequests", \
+                                       "TotalUnlockRequests", \
+                                       "TraceRequestsPersec", \
+                                       "UnlockRequestsPersec"], \
+                                      provider="64", timeout_duration=50)
+            metrics.sample()
+            ts = int(time.time())
 
-        for metric in metrics:
-            for key, value in metric.iteritems():
-                if isinstance(value, numbers.Number):
-                    self._readq.nput("webservice.%s %d %f" % (key, ts, value))
+            for metric in metrics:
+                iis_alive = True
+                for key, value in metric.iteritems():
+                    if isinstance(value, numbers.Number):
+                        self._readq.nput("webservice.%s %d %f" % (key, ts, value))
+        except:
+            iis_alive = False
 
+        if iis_alive:
+            self._readq.nput("iis.state %d %f" % (int(time.time()), 0))
+        else :
+            self._readq.nput("iis.state %d %f" % (int(time.time()), 1))
 
 if __name__ == "__main__":
     iisstats_inst = WindowsIisstats(None, None, Queue())
