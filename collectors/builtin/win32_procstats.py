d8fcda86ae69d029ccb23ce3c3621f66755a1f94
Lzy <347316109@qq.com>
2017-08-08 12:10:29 +0800

    修改名字

--- collectors/builtin/win32_procstats.py
+++ collectors/builtin/win32_procstats.py
@@ -29,10 +29,10 @@ from collectors.lib.collectorbase import CollectorBase
 import checks.system.win32 as w32
 
 
-class Win32Procstats3(CollectorBase):
+class Win32Procstats(CollectorBase):
 
     def __init__(self, config, logger, readq):
-        super(Win32Procstats3, self).__init__(config, logger, readq)
+        super(Win32Procstats, self).__init__(config, logger, readq)
 
 
     def __call__(self):
@@ -48,5 +48,5 @@ class Win32Procstats3(CollectorBase):
 
 
 if __name__ == "__main__":
-    procstats3_inst = Win32Procstats3(None, None, Queue())
+    procstats3_inst = Win32Procstats(None, None, Queue())
     procstats3_inst()
