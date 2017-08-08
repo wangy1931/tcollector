67f40433d8141d873bbd092b48012ad635d0ee96
Lzy <347316109@qq.com>
2017-08-08 11:54:28 +0800

    windows打包程序

--- deploy/windows/windows_build.bat
+++ deploy/windows/windows_build.bat
@@ -1,14 +1,30 @@
 @echo off
+copy windows_setup.py ..\..\
+cd ..\..\
 mkdir logs
 echo "" >> logs\collector.log
 mkdir runs
 echo "" >> runs\collector.pid
+
+python windows_setup.py build
+cd build\
+mkdir agent
+xcopy  /s exe.win-amd64-2.7  agent
+cd agent\collectors
+rd /s /q 0 30 300
+cd ..\..
+del  /q ..\deploy\releases\windows\cloudwiz-agent-amd64.zip
+C:\"Program Files"\7-Zip\7z a  -r ..\deploy\releases\windows\cloudwiz-agent-amd64.zip agent
+cd ..\deploy\workspace
+xcopy  /s ..\windows\user.conf filebeat-5.4.2-windows-x86_64
+xcopy /s ..\windows\common.conf filebeat-5.4.2-windows-x86_64
+xcopy /s ..\windows\filebeat_template.yml filebeat-5.4.2-windows-x86_64
+C:\"Program Files"\7-Zip\7z a  -r ..\releases\windows\cloudwiz-agent-amd64.zip filebeat-5.4.2-windows-x86_64
+cd ..\windows
+C:\"Program Files"\7-Zip\7z a  -r ..\releases\windows\cloudwiz-agent-amd64.zip altenv
+cd ..\..
 rd /s /q build
-rd /s /q dist
-python win_setup.py bdist_msi
-rd /s /q deploy\releases\windows\dist
-move  dist deploy\releases\windows
-rd /s /q build
-rd /s /q dist
 rd /s /q logs
 rd /s /q runs
+del  /q windows_setup.py
+cd deploy/windows
