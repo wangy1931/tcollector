90b049dbf2b1d61cdcbf11c3f3eeaa5851fc6b74
Lzy <347316109@qq.com>
2017-08-10 12:33:26 +0800

    添加默认覆盖

--- deploy/windows/windows_build.bat
+++ deploy/windows/windows_build.bat
@@ -16,9 +16,9 @@ cd ..\..
 del  /q ..\deploy\releases\windows\cloudwiz-agent-amd64.zip
 C:\"Program Files"\7-Zip\7z a  -r ..\deploy\releases\windows\cloudwiz-agent-amd64.zip agent
 cd ..\deploy\workspace
-xcopy  /s ..\windows\user.conf filebeat-5.4.2-windows-x86_64
-xcopy /s ..\windows\common.conf filebeat-5.4.2-windows-x86_64
-xcopy /s ..\windows\filebeat_template.yml filebeat-5.4.2-windows-x86_64
+xcopy  /s /y ..\windows\user.conf filebeat-5.4.2-windows-x86_64
+xcopy /s /y ..\windows\common.conf filebeat-5.4.2-windows-x86_64
+xcopy /s /y ..\windows\filebeat_template.yml filebeat-5.4.2-windows-x86_64
 C:\"Program Files"\7-Zip\7z a  -r ..\releases\windows\cloudwiz-agent-amd64.zip filebeat-5.4.2-windows-x86_64
 cd ..\windows
 C:\"Program Files"\7-Zip\7z a  -r ..\releases\windows\cloudwiz-agent-amd64.zip altenv
