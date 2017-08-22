cf7b45629da67fbd063a32ad9b8d128208d1e7d3
Lzy <347316109@qq.com>
2017-08-22 19:01:19 +0800

    fix bug

--- collectors/builtin/win32_events.py
+++ collectors/builtin/win32_events.py
@@ -3,9 +3,12 @@ import codecs
 import os
 import time
 
+from datetime import datetime, timedelta
+import pythoncom
 import wmi
 
 from collectors.lib.collectorbase import CollectorBase
+from collectors.lib.utils import TestQueue
 
 record_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../event_last_query.conf')
 log_file_path = 'c:\\opt\\cloudwiz-agent\\altenv\\var\\log\\events.log'
@@ -17,11 +20,19 @@ class Win32Events(CollectorBase):
 
     def __call__(self):
         try:
+            pythoncom.CoInitialize()
+
             last_query = self.get_last_query()
             c = wmi.WMI()
             wql = 'SELECT * FROM Win32_NTLogEvent WHERE EventType < 3 '
             if last_query:
                 wql += "AND TimeGenerated > '" + last_query + "'"
+            else:
+                d = datetime.now()
+                t = timedelta(days=15)
+                start_time = d - t
+                wmi_time = wmi.from_time(start_time.year, start_time.month, start_time.day, start_time.hour, start_time.minute, start_time.second, 0, 0)
+                wql += "AND TimeGenerated > '" + wmi_time + "'"
 
             timestamp = 0
             time_t = None
@@ -51,6 +62,8 @@ class Win32Events(CollectorBase):
         except Exception as e:
             self.log_error('cannot send host scan result to alertd %s' % e)
             self._readq.nput('scan.state %s %s' % (int(time.time()), '1'))
+        finally:
+            pythoncom.CoUninitialize()
 
     def fix_date(self, wmi_time):
         time_tuple = wmi.to_time(wmi_time)
@@ -75,3 +88,7 @@ class Win32Events(CollectorBase):
         # mode 'w' will truncate the file when open
         with open(record_file_path, 'w') as f:
             f.write(time_t)
+
+if __name__ == "__main__":
+    memstats3_inst = Win32Events(None, None, TestQueue())
+    memstats3_inst.__call__()
