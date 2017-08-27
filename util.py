cca6511b4258f0fbd3572975693b42868f433fde
Lzy <347316109@qq.com>
2017-08-27 16:34:34 +0800

    删除没有使用的code

--- util.py
+++ util.py
@@ -10,22 +10,11 @@ import uuid
 
 # 3p
 import yaml  # noqa, let's guess, probably imported somewhere
-try:
-    from yaml import CLoader as yLoader
-    from yaml import CDumper as yDumper
-except ImportError:
-    # On source install C Extensions might have not been built
-    from yaml import Loader as yLoader  # noqa, imported from here elsewhere
-    from yaml import Dumper as yDumper  # noqa, imported from here elsewhere
 
 # These classes are now in utils/, they are just here for compatibility reasons,
 # if a user actually uses them in a custom check
 # If you're this user, please use utils/* instead
 # FIXME: remove them at a point (6.x)
-from utils.pidfile import PidFile  # noqa, see ^^^
-from utils.platform import Platform, get_os # noqa, see ^^^
-from utils.proxy import get_proxy # noqa, see ^^^
-from utils.timer import Timer # noqa, see ^^^
 
 COLON_NON_WIN_PATH = re.compile(':(?!\\\\)')
 
@@ -34,45 +23,6 @@ log = logging.getLogger(__name__)
 NumericTypes = (float, int, long)
 
 
-def plural(count):
-    if count == 1:
-        return ""
-    return "s"
-
-def get_uuid():
-    # Generate a unique name that will stay constant between
-    # invocations, such as platform.node() + uuid.getnode()
-    # Use uuid5, which does not depend on the clock and is
-    # recommended over uuid3.
-    # This is important to be able to identify a server even if
-    # its drives have been wiped clean.
-    # Note that this is not foolproof but we can reconcile servers
-    # on the back-end if need be, based on mac addresses.
-    return uuid.uuid5(uuid.NAMESPACE_DNS, platform.node() + str(uuid.getnode())).hex
-
-
-def headers(agentConfig, **kwargs):
-    # Build the request headers
-    res = {
-        'User-Agent': 'Datadog Agent/%s' % agentConfig['version'],
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'text/html, */*',
-    }
-    if 'http_host' in kwargs:
-        res['Host'] = kwargs['http_host']
-    return res
-
-
-def windows_friendly_colon_split(config_string):
-    '''
-    Perform a split by ':' on the config_string
-    without splitting on the start of windows path
-    '''
-    if Platform.is_win32():
-        # will split on path/to/module.py:blabla but not on C:\\path
-        return COLON_NON_WIN_PATH.split(config_string)
-    else:
-        return config_string.split(':')
 
 
 def cast_metric_val(val):
@@ -93,67 +43,5 @@ def cast_metric_val(val):
 _IDS = {}
 
 
-def get_next_id(name):
-    global _IDS
-    current_id = _IDS.get(name, 0)
-    current_id += 1
-    _IDS[name] = current_id
-    return current_id
 
 
-def check_yaml(conf_path):
-    with open(conf_path) as f:
-        check_config = yaml.load(f.read(), Loader=yLoader)
-        assert 'init_config' in check_config, "No 'init_config' section found"
-        assert 'instances' in check_config, "No 'instances' section found"
-
-        valid_instances = True
-        if check_config['instances'] is None or not isinstance(check_config['instances'], list):
-            valid_instances = False
-        else:
-            for i in check_config['instances']:
-                if not isinstance(i, dict):
-                    valid_instances = False
-                    break
-        if not valid_instances:
-            raise Exception('You need to have at least one instance defined in the YAML file for this check')
-        else:
-            return check_config
-
-def config_to_yaml(config):
-    '''
-    Convert a config dict to YAML
-    '''
-    assert 'init_config' in config, "No 'init_config' section found"
-    assert 'instances' in config, "No 'instances' section found"
-
-    valid_instances = True
-    if config['instances'] is None or not isinstance(config['instances'], list):
-        valid_instances = False
-    else:
-        yaml_output = yaml.safe_dump(config, default_flow_style=False)
-
-    if not valid_instances:
-        raise Exception('You need to have at least one instance defined in your config.')
-
-    return yaml_output
-
-"""
-Iterable Recipes
-"""
-
-def chunks(iterable, chunk_size):
-    """Generate sequences of `chunk_size` elements from `iterable`."""
-    iterable = iter(iterable)
-    while True:
-        chunk = [None] * chunk_size
-        count = 0
-        try:
-            for _ in range(chunk_size):
-                chunk[count] = iterable.next()
-                count += 1
-            yield chunk[:count]
-        except StopIteration:
-            if count:
-                yield chunk[:count]
-            break
